fdfb50660d92210b3a7c67064fae293dee036867
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2024-09-13 22:24:19 -0400

    all works are done

--- src/main/java/com/example/csc311_lab02/LoanCalculator.java
+++ src/main/java/com/example/csc311_lab02/LoanCalculator.java
@@ -3,6 +3,7 @@ package com.example.csc311_lab02;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,9 @@ public class LoanCalculator extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LoanCalculator.class.getResource("LoanCalculator-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        AnchorPane root = new AnchorPane();
+        fxmlLoader.setRoot(root); // set the root as Anchor Pane type
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Loan Calculator");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/csc311_lab02/LoanController.java
+++ src/main/java/com/example/csc311_lab02/LoanController.java
@@ -1,14 +1,85 @@
 package com.example.csc311_lab02;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class LoanController {
     @FXML
-    private Label welcomeText;
+    private TextField totalPayment;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField monthlyPayment;
+
+    @FXML
+    private TextField annualInterestRate;
+
+    @FXML
+    private TextField numberOfYears;
+
+    @FXML
+    private TextField loanAmount;
+
+    @FXML
+    private Text errorMessage;
+
+    @FXML
+    void loanCalculator(ActionEvent event) {
+        calculateLoan();
     }
+
+    private void calculateLoan(){
+        double r =getAnnualInterestRate(); // r = Annual Interest Rate
+        int t =getNumberOfYears();         // t = number of years
+        double P =getLoanAmount();         // P = amount of loan
+        double R;                          // R = monthly payment
+        double T;                          // T = total payment
+        int n = 12;                        // n = 12 monthly payment per years
+
+        R = (P*(r/n))/(1-Math.pow((1+(r/n)),-n*t)); // formula for monthly payment
+        T = R*n*t; // total payment
+
+        setMonthlyPayment(R); // print Monthly Payment
+        setTotalPayment(T); // print Total Payment
+    }
+
+
+    private void setTotalPayment(double totalPayment) {
+        //shows the result in TextField with $ and remain as 2 decimal
+        this.totalPayment.setText("$"+String.format("%.2f", totalPayment));
+    }
+
+    private void setMonthlyPayment(double monthlyPayment) {
+        //shows the result in TextField with $ and remain as 2 decimal
+        this.monthlyPayment.setText("$"+String.format("%.2f", monthlyPayment));
+    }
+
+    private double getAnnualInterestRate() {
+        return getValue(annualInterestRate)/100; // % to decimal number
+    }
+
+    private int getNumberOfYears() {
+        return (int)getValue(numberOfYears); // integer value for years
+    }
+
+    private double getLoanAmount() {
+        return getValue(loanAmount); // double value
+    }
+
+    // a tool class to using duplicate code
+    private double getValue(TextField value){
+        String str = value.getText(); // get value from different text field
+        // try to converse the string value to be digit value,
+        // if the input not digit that will clear and let user enter again
+        try{
+            errorMessage.setText(""); // Display message is empty for initial settings
+            return Double.parseDouble(str);
+        }catch (NumberFormatException e){
+            value.clear(); // clear the input
+            errorMessage.setText("Error!!! Please Enter digit values!!");
+        }
+        return 0;
+    }
+
 }
